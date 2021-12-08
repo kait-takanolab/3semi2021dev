858ed61b9f1a8339cb9bc8dd26c81271f812cc47
ktakano <takano@ic.kanagawa-it.ac.jp>
2021-12-08 12:27:42 +0900

    SQLを修正した。
    music.sql: foreign keyにreferencesを追加した。
    
    user.sql: idにauto_incrementを設定した。
    テスト用のinsert文を追加した。

--- db/music.sql
+++ db/music.sql
@@ -1,8 +1,8 @@
-CREATE TABLE music{
-	id int,
+CREATE TABLE music (
+	id int PRIMARY KEY,
 	uid int,
 	file varchar(256),
 	spectrogram varchar(256),
 
-	FOREIGN KEY(uid)
-}
+	FOREIGN KEY(uid) REFERENCES user(id)
+);
--- db/user.sql
+++ db/user.sql
@@ -1,5 +1,8 @@
 CREATE TABLE user (
-id    INT PRIMARY KEY,
+id    INT PRIMARY KEY AUTO_INCREMENT,
 mail     VARCHAR(256),
 name     VARCHAR(256)
 );
+
+-- For test
+-- INSERT INTO user (mail, name) VALUES('s1921121', 'masuda')
